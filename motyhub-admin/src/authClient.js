06d2ab4e1ef8bb6e6a2ecc876773738ac7dc2bf7
kayhide <kayhide@gmail.com>
2017-08-28 18:37:26 +0900

    update admin to generate jwt.

--- motyhub-admin/src/authClient.js
+++ motyhub-admin/src/authClient.js
@@ -1,9 +1,18 @@
+import KJUR from 'jsrsasign';
 import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR } from 'admin-on-rest';
 
 export default (type, params) => {
   if (type === AUTH_LOGIN) {
     const { username, password } = params;
-    localStorage.setItem('token', password);
+    const header = { alg: 'HS256', typ: 'JWT' };
+    const payload = { role: username };
+    const token = KJUR.jws.JWS.sign(
+      header.alg,
+      JSON.stringify(header),
+      JSON.stringify(payload),
+      { utf8: password }
+    );
+    localStorage.setItem('token', token);
     return Promise.resolve();
   }
   if (type === AUTH_LOGOUT) {
